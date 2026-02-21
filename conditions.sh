963c0eccec4ec45f7c7b9166d27d63d0ed4d5125
Sumatej Sunku <sumatejdevops@gmail.com>
2026-02-21 18:48:46 +0530

    conditions example

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-Number=$1
+#Number=$1
 
-if [ $Number -lt 10 ]; then
-    echo "The given number is $Number and it is less than 10"
+#if [ $Number -lt 10 ]; then
+#    echo "The given number is $Number and it is less than 10"
+#else
+#    echo "The given number is $Number and it is greater than or equal to 10"
+#fi
+
+echo "Enter the number to check if it is EVEN or ODD:"
+read Num
+
+if [ $(($Num % 2)) -eq 0 ]; then
+    echo "The given number is EVEN"
 else
-    echo "The given number is $Number and it is greater than or equal to 10"
+    echo "The given number is ODD"
 fi
