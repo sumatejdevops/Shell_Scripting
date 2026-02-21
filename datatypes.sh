67cf30467d67d8edea2705018441449eb2060248
Sumatej Sunku <sumatejdevops@gmail.com>
2026-02-21 17:46:31 +0530

    datatypes example

--- datatypes.sh
+++ datatypes.sh
@@ -6,6 +6,6 @@ Name=Sumatej
 SUM=$(($Number1+$Number2))
 echo "SUM is: ${SUM}"
 
-Names=("Sumatej", "Ravitej", "Sunku")
+Names=("Sumatej" "Ravitej" "Sunku")
 echo "All Leaders: ${Names[@]}"
 echo "First Leader: ${Names[0]}"
