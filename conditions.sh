63c4b7fe8a848b1877d415bf8d1df472efd1edde
Sumatej Sunku <sumatejdevops@gmail.com>
2026-02-21 18:39:24 +0530

    conditions example

--- conditions.sh
+++ conditions.sh
@@ -5,5 +5,5 @@ Number=$1
 if [ $Number -lt 10 ]; then
     echo "The given number $Number is less than 10"
 else
-    echo "The given number $Number is greater than 10"
+    echo "The given number $Number is greater than or equal to 10"
 fi
