479708537bb9b203fe5998896661b7db1808f4dc
Sumatej Sunku <sumatejdevops@gmail.com>
2026-02-21 23:17:40 +0530

    conditions example

--- installation_conditions.sh
+++ installation_conditions.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-
+echo "$USER_ID"
 if [ $USER_ID -ne 0 ]; then
     echo "Error:: Please run the script with root privelege"
     exit 1
 fi
 
-dnf install mysql -y
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Error:: Installation of $2 is failed"
+        exit 1
+    else
+        echo "$2 has been successfully installed"
+    fi
+}
 
-if [ $? -ne 0 ]; then
-    echo "Error MySQL installation failed"
-    exit 1
-else
-    echo "mysql has been installed successfully"
-fi
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install ngnix -y
+VALIDATE $? "Ngnix"
+
+dnf install python -y
+VALIDATE $? "Python"
+
+
